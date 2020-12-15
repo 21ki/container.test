417fdfaec49e01c3001fd40a81f0f68e1f0a221f
zxm <zhaoxuemin@jdjt.net>
2020-12-15 13:35:26 +0800

    init

---
+++ src/main/java/com/sjlh/container/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sjlh.container;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello the world!");
+	}
+
+}
